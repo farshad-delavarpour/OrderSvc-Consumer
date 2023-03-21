f2523d9e9385d96c7eb5154821397701023957aa
Farshad <farshaddelavarpour@gmail.com>
2023-03-21 17:04:42 +0330

    Add OrderSvc-Test Project

---
+++ OrderSvc-Consumer/Models/DiscountModel.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderSvc_Consumer.Models
+{
+    public class DiscountModel
+    {
+        public double CustomerRating { get; set; }
+        public double AmountToDiscount { get; set; }
+    }
+}
